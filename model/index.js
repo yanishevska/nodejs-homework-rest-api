8a4987df57204658374a6c62e924e6b5385cef32
yanishevska <yanishevska1989@gmail.com>
2021-12-18 10:35:06 +0100

    create branch

--- model/index.js
+++ model/index.js
@@ -1,6 +1,3 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
-
 const listContacts = async () => {}
 
 const getContactById = async (contactId) => {}
@@ -18,3 +15,4 @@ module.exports = {
   addContact,
   updateContact,
 }
+
